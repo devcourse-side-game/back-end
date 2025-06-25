b4c3e085ce8090ec262bc7fff4453fe96a5034b8
Beomseok OH <ilsny7@me.com>
2025-06-25 11:11:16 +0900

    chore: 컬럼 컨벤션 변경 및 스웨거 추가

--- src/games/entities/game.entity.ts
+++ src/games/entities/game.entity.ts
@@ -8,36 +8,54 @@ import {
 	OneToMany,
 } from 'typeorm';
 import { Party } from '../../parties/entities/party.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('games')
 @Index('idx_name_platform', ['name', 'platforms'])
 export class Game {
-	@PrimaryGeneratedColumn()
+	@ApiProperty({ example: 1, description: '게임 고유 ID' })
+	@PrimaryGeneratedColumn({ name: 'id' })
 	id: number;
 
-	@Column({ length: 255 })
+	@ApiProperty({ example: 'League of Legends', description: '게임 이름' })
+	@Column({ name: 'name', length: 255 })
 	name: string;
 
-	@Column({ type: 'enum', enum: ['steam', 'riot', 'blizzard', 'custom'] })
+	@ApiProperty({
+		example: 'steam',
+		description: '플랫폼 구분',
+		enum: ['steam', 'riot', 'blizzard', 'custom'],
+	})
+	@Column({
+		type: 'enum',
+		enum: ['steam', 'riot', 'blizzard', 'custom'],
+		name: 'platforms',
+	})
 	platforms: 'steam' | 'riot' | 'blizzard' | 'custom';
 
+	@ApiProperty({ example: 570, description: 'Steam App ID', required: false, nullable: true })
 	@Index({ unique: true })
-	@Column({ type: 'int', unsigned: true, nullable: true })
+	@Column({ type: 'int', unsigned: true, nullable: true, name: 'steam_app_id' })
 	steamAppId?: number;
 
-	@Column({ length: 255, nullable: true })
+	@ApiProperty({ example: 'https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg', description: '게임 배너 이미지 URL', required: false, nullable: true })
+	@Column({ length: 255, nullable: true, name: 'banner_url' })
 	bannerUrl?: string;
 
-	@Column({ type: 'tinyint', default: 1 })
+	@ApiProperty({ example: true, description: '활성화 여부' })
+	@Column({ type: 'tinyint', default: 1, name: 'is_active' })
 	isActive: boolean;
 
-	@CreateDateColumn()
+	@ApiProperty({ example: '2024-06-23T12:34:56.000Z', description: '생성일' })
+	@CreateDateColumn({ name: 'created_at' })
 	createdAt: Date;
 
-	@UpdateDateColumn()
+	@ApiProperty({ example: '2024-06-23T12:34:56.000Z', description: '수정일' })
+	@UpdateDateColumn({ name: 'updated_at' })
 	updatedAt: Date;
 
-	@Column({ length: 255, unique: true })
+	@ApiProperty({ example: 'league-of-legends', description: '슬러그(고유 식별자)' })
+	@Column({ length: 255, unique: true, name: 'slug' })
 	slug: string;
 
 	@OneToMany(() => Party, (party) => party.game)
